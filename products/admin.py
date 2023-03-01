2a3c23017f4cc1c597aa5b5557d4a08ba1ea7dcf
MissBlumarine <miss_blumarine01@mail.ru>
2023-03-01 07:42:29 +0300

    last_chandges

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from products.models import ProductCategory, Product
 
 # Register your models here.
+# admin.site.register(Product)
+admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'category', 'price'
+    list_display_links = 'name',
+    ordering = 'id',
--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory
 
 # Create your views here.
+# функции = контроллеры = вьюхи
+
+
+def index(request):
+    context = {
+        'title': 'Test Title',
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'category': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
+
+
