1901b915f2b815a0ce2c510478b623bbab02825e
MissBlumarine <miss_blumarine01@mail.ru>
2023-03-12 00:59:32 +0300

    + приложение users

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# users
+
+AUTH_USER_MODEL = 'users.User'
