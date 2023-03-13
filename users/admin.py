b334dd2a988332f264665f57f99de15f65430328
MissBlumarine <miss_blumarine01@mail.ru>
2023-03-13 12:21:17 +0300

    + last_changes

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from users.models import User
 
-admin.site.register(User)
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = 'id', 'username', 'first_name', 'last_name', 'email'
+    list_display_links = 'username',
+    ordering = 'id',
